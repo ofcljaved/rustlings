71bd9e5dedb70c722956ac950c802963d2316aa4
ofcljaved <javedofficialme@gmail.com>
2024-10-26 14:55:51 +0530

    chore: iterator with closure

--- exercises/18_iterators/iterators2.rs
+++ exercises/18_iterators/iterators2.rs
@@ -7,7 +7,7 @@ fn capitalize_first(input: &str) -> String {
     let mut chars = input.chars();
     match chars.next() {
         None => String::new(),
-        Some(first) => todo!(),
+        Some(first) => first.to_ascii_uppercase().to_string() + chars.as_str(),
     }
 }
 
@@ -15,14 +15,16 @@ fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    // ???
+    let word_iter = words.iter();
+    word_iter.map(|w| capitalize_first(w)).collect()
 }
 
 // TODO: Apply the `capitalize_first` function again to a slice of string
 // slices. Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 fn capitalize_words_string(words: &[&str]) -> String {
-    // ???
+    let word_iter = words.iter();
+    word_iter.map(|w| capitalize_first(w)).collect()
 }
 
 fn main() {
